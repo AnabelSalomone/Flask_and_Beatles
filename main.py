00d1962212c71d896057a468b005fa065f0e689e
AnabelSalomone <anabel.salomone@gmail.com>
2020-11-01 22:36:07 +0100

    add youtube service

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from decouple import config
+from googleapiclient.discovery import build
 import json
 
 app = Flask(__name__)
+SECRET_KEY=config('API_KEY')
+
+youtube = build('youtube', 'v3', developerKey=SECRET_KEY)
 
 @app.route("/")
 def beatles():
@@ -21,6 +26,24 @@ def songs(id):
             return render_template("songs.html", content=obj["tracks"])
         
     return "No data found"
+
+@app.route('/video/<query>')
+def video(query):
+        req = youtube.search().list(
+            part="snippet",
+            maxResults=1,
+            q="Beatles " + query
+        )
+
+        response = req.execute()
+        video = []
+
+        for search_result in response.get("items", []):
+            if search_result["id"]["kind"] == "youtube#video":
+                video.append(search_result["id"]["videoId"])
+
+        return render_template("video.html", url=video)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
